1c547e733a1c859f3c58fbb630a06886402e750c
divine388 <rishabhmastermind720@gmail.com>
2026-04-02 11:31:03 +0530

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,11 @@
+import Layout from './layout';
+
+const HomePage: React.FC = () => {
+  return (
+    <Layout>
+      <h2>Welcome to Divine Essentials</h2>
+    </Layout>
+  );
+};
+
+export default HomePage;
