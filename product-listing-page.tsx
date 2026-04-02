a5813ba3e94510090fe4a5bfed24bc3e125429ac
divine388 <rishabhmastermind720@gmail.com>
2026-04-02 11:31:56 +0530

    Create product-listing-page.tsx

---
+++ product-listing-page.tsx
@@ -0,0 +1,12 @@
+import Layout from './layout';
+
+const ProductListingPage: React.FC = () => {
+  return (
+    <Layout>
+      <h2>Product Listing</h2>
+      {/* Placeholder for product items */}
+    </Layout>
+  );
+};
+
+export default ProductListingPage;
