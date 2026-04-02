75e4aa1d486888734542321270d68b385ee6b4e2
divine388 <rishabhmastermind720@gmail.com>
2026-04-02 11:32:46 +0530

    Create product-details-page.tsx

---
+++ product-details-page.tsx
@@ -0,0 +1,12 @@
+import Layout from './layout';
+
+const ProductDetailsPage: React.FC = () => {
+  return (
+    <Layout>
+      <h2>Product Details</h2>
+      {/* Placeholder for product details and shopping cart */}
+    </Layout>
+  );
+};
+
+export default ProductDetailsPage;
